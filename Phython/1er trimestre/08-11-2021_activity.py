f24934e2f0ed8291b3cf824cce2afe3418c2ae4b
Elisavvm <104370090+Elisavvm@users.noreply.github.com>
2022-06-03 14:01:17 -0500

    Add files via upload

---
+++ Phython/1er trimestre/08-11-2021_activity.py
@@ -0,0 +1,19 @@
+print('What is your name?')
+Name=input()
+print('Welcome', Name)
+
+print('In which school are you?')
+school=input()
+print('nice, me too')
+
+print('In which grade are you?')
+grade=input()
+
+print('In which group are you?')
+group=input()
+
+print('What is your favorite food?')
+food=input()
+
+print('Tell me something random:')
+random=input()
