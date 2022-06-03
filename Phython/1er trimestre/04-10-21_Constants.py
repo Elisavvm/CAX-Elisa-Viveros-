4b1795f08913a0d17200db0248fc46f39613c309
Elisavvm <104370090+Elisavvm@users.noreply.github.com>
2022-06-03 13:51:50 -0500

    Add files via upload

---
+++ Phython/1er trimestre/04-10-21_Constants.py
@@ -0,0 +1,29 @@
+#heyyyyy
+
+x=10
+
+jamon=10
+
+justo=2345
+
+excusa='puedo ir al baño?'
+
+print(x)
+print(jamon)
+print(excusa)
+
+c0m03st4n=20
+print(c0m03st4n) 
+print('el precio del jamon es:',jamon)
+
+#rehacer la presentacion ocupando variables
+name='Elisa Viveros'
+grade='9th grade'
+group='C'
+favfood='pizza'
+something='i love sports'
+print('my name is:',name)
+print('im on ',grade )
+print('on group',group)
+print('my favorite food is:',favfood)
+print(something)
