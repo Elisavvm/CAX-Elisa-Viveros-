20efe92c8f03db556166ae978835b678817cf76a
Elisavvm <104370090+Elisavvm@users.noreply.github.com>
2022-06-03 13:58:26 -0500

    Add files via upload

---
+++ Phython/1er trimestre/18-10-2021_digitalcitizenship.py
@@ -0,0 +1,14 @@
+print("-How can we act with empathy and positivity when we're online?")
+print("-Trying to help other people by doing good commentaries")
+Digital_dilemma= "-Digital dilemma is a situation in digital life where it's not always clear what's best to do"
+print(Digital_dilemma)
+opinion= "-In my opinion the girl act in a bad way because she humiliated the boy in internet and she didn't realized the consequenses"
+print(opinion)
+identify= "-The boy and the girl were involved and the dilemma was that the girl published the letter"
+print(identify)
+feel="-I think that the boy was feeling bad and the girl was feeling happy, it was difficult because the boy was bullied and the girl was feeling guilty "
+print(feel)
+imagine="- They should have talked first, The girl shouldn't have published the letter, The girl shouldn't have humilliated the boy"
+print(imagine)
+say="- if the girl shouldn't have published the letter, no one would have bothered the boy "
+print(say) 
