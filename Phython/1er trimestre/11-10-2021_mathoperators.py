47fcca87aefb60b1571be990e269187f5abffa79
Elisavvm <104370090+Elisavvm@users.noreply.github.com>
2022-06-03 13:56:16 -0500

    Add files via upload

---
+++ Phython/1er trimestre/11-10-2021_mathoperators.py
@@ -0,0 +1,66 @@
+trombones76 ='big parade'
+more = 1000000
+classe = 'Advanced Theoretical Zymurgy'
+
+a=6
+b=2
+
+#add
+total=a+b
+print(total)
+
+#substraction
+total=a-b
+print(total)
+
+#multiplication
+total=a*b
+print(total)
+
+#division
+total=a/b
+print(total)
+
+#exponents
+total=a**b
+print(total)
+
+#percentage
+E1= 98.5
+E2= 99.2
+E3= 97.5
+total= E1+E2+E3
+esp=total/3
+print(esp)
+
+E1= 99.6
+E2= 99.8
+E3= 96.5
+total= E1+E2+E3
+LA=total/3
+print(LA)
+
+E1= 97.6
+E2= 95.9
+E3= 98.3
+total= E1+E2+E3
+SS=total/3
+print(SS)
+
+E1= 98.8
+E2= 95.9
+E3= 94.7
+total= E1+E2+E3
+science=total/3
+print(science)
+
+E1= 94.7
+E2= 99.9
+E3= 97.8
+total= E1+E2+E3
+math=total/3
+print(math)
+
+total=esp+LA+SS+science+math
+materias=total/5
+print(materias)
