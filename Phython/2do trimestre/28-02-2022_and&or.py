124e7bea050d0d8e43bd894d2679416a89041ef7
Elisavvm <104370090+Elisavvm@users.noreply.github.com>
2022-06-27 10:01:55 -0500

    Add files via upload

---
+++ Phython/2do trimestre/28-02-2022_and&or.py
@@ -0,0 +1,17 @@
+#and 
+#meaning: True if both the operands are true
+#Example: x&y
+#Code:
+x= True
+y= False
+
+print('X and Y is', x and y)
+
+#or
+#Meaning: True if either of the operands is true
+#Example: XorY
+#Code:
+x= True
+y= False
+
+print('X and Y is', x or y)
