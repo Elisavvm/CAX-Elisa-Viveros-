7ea0dc34139b3757556a1223983053936f848965
Elisavvm <104370090+Elisavvm@users.noreply.github.com>
2022-06-27 09:14:30 -0500

    Add files via upload

---
+++ Phython/2do trimestre/10-12-2021_activity.py
@@ -0,0 +1,27 @@
+#crear un codigo que capture 20 nombres, despues de eso aprezca un mensaje de clase llena
+a=1
+while(a<=20):
+  print("Add your name")
+  name=input()
+  a=a+1
+  print('names:',a-1,'names')
+#crear un codigo que solo te permita vender 15 boletos de una rifa
+a=1
+while(a<=15):
+  print('ticket',a+1,'ticket sold')
+  ticket=input()
+  a=a+1
+
+#crear un codigo que pida una contraseña y no te deje avanzar hasta que ingreses el password correcto
+a=1
+while(a!=897):
+ a=int(input())
+
+print('correcto')  
+
+#modifica el codigo antetior para que una vez que le atines a la contraseña, infinitamente diga: te hemos hackeado
+a=1
+while(a==897):
+ a=int(input())
+
+print('te hemos hackeado')  
