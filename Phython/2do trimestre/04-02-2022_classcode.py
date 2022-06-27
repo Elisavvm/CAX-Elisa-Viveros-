0980c60f631285878179bb2d8b8f381ca0a9a983
Elisavvm <104370090+Elisavvm@users.noreply.github.com>
2022-06-27 10:00:31 -0500

    Add files via upload

---
+++ Phython/2do trimestre/04-02-2022_classcode.py
@@ -0,0 +1,11 @@
+numero=int(input("ingresa un numero: "))
+
+if(numero<8):
+  print("Te falta chav@")
+
+#Todo lo demás
+#Para que haya un else necesitas un IF
+else:
+  print("Te sobra chav@")
+
+#Topic Review: un codigo que verifique que la contraseña ingresada es o no correcta
