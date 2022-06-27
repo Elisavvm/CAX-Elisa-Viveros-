fa7a0db8adb4089a74526923b7642d10689f9860
Elisavvm <104370090+Elisavvm@users.noreply.github.com>
2022-06-27 10:01:06 -0500

    Add files via upload

---
+++ Phython/2do trimestre/04-02-2022_topicreview.py
@@ -0,0 +1,10 @@
+#Topic Review: un codigo que verifique que la contraseña ingresada es o no correcta
+print("La contrasena es de 2 digitos")
+contra=int(input("ingresa la contrasena: "))
+if (contra==88):
+  print("Correcto")
+
+else:
+  print("Incorrecto")
+  print("Intenta de nuevo")
+  
