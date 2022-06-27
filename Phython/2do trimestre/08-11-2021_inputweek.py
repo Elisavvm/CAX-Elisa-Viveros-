973e7fc27798eb711aa2369f7ab52302b00b58ed
Elisavvm <104370090+Elisavvm@users.noreply.github.com>
2022-06-27 08:37:36 -0500

    Add files via upload

---
+++ Phython/2do trimestre/08-11-2021_inputweek.py
@@ -0,0 +1,33 @@
+print('What is your name?')
+Name=input()
+print('Welcome', Name)
+
+print('In which school are you?')
+school=input()
+
+print('In which grade are you?')
+grade=input()
+
+print('How old are you?')
+old=input()
+
+print('What is your favorite food?')
+food=input()
+
+print('In which country do you live?:')
+country=input()
+
+print('Where do you live?')
+place=input()
+
+print('What is your favorite place in the world?')
+world=input()
+
+print('What is your favorite movie?')
+movie=input()
+
+
+print('How are you feeling today?')
+feeling=input()
+
+print('Thanks for anwering!')
