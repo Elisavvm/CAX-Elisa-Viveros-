d5cfffe3881c1ce9c88a99334f3e13b4291b9cb2
Elisavvm <104370090+Elisavvm@users.noreply.github.com>
2022-06-27 09:16:48 -0500

    Add files via upload

---
+++ Phython/2do trimestre/10-01-2022_condicionales.py
@@ -0,0 +1,19 @@
+#if
+
+x=5
+if x<10:
+  print('smaller')
+if x>20:
+  print('bigger')
+
+print('finis')
+
+------------------------
+
+x=int(input())
+if x<10:
+  print('smaller')
+if x>20:
+  print('bigger')
+
+print('finis')
