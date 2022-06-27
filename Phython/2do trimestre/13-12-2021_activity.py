e0dff101802ff060cbdbb0e0f846339aebfa4482
Elisavvm <104370090+Elisavvm@users.noreply.github.com>
2022-06-27 09:15:21 -0500

    Add files via upload

---
+++ Phython/2do trimestre/13-12-2021_activity.py
@@ -0,0 +1,13 @@
+#crear un codigo que pida una contraseña y no te deje avanzar hasta que ingreses el password correcto
+a=1
+while(a!=897):
+ a=int(input())
+
+print('correcto')  
+
+#modifica el codigo antetior para que una vez que le atines a la contraseña, infinitamente diga: te hemos hackeado
+a=1
+while(a==897):
+ a=int(input())
+
+print('te hemos hackeado')  
